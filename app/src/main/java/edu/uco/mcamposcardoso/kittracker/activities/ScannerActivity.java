c8678c264f632716eab80307148a7d2cf00ee78e
Matheus Campos Cardoso <its.matheus3@gmail.com>
2017-05-09 21:42:14 -0300

    Remove unused code

--- app/src/main/java/edu/uco/mcamposcardoso/kittracker/activities/ScannerActivity.java
+++ app/src/main/java/edu/uco/mcamposcardoso/kittracker/activities/ScannerActivity.java
@@ -52,38 +52,6 @@ public class ScannerActivity extends FragmentActivity implements SampleFragment.
 
         FragmentManager fm = getSupportFragmentManager();
         mScannerFragment = (BarCodeScannerFragment) fm.findFragmentById(R.id.scanner_fragment);
-
-/*    layoutContent = (LinearLayout) findViewById(R.id.layout_content);
-
-    final ViewTreeObserver observer = layoutContent.getViewTreeObserver();
-
-    observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-      @Override
-      public void onGlobalLayout() {
-        // We're assuming that the other layout is under the scanner
-        int activityWidth = layoutContent.getWidth();
-        int activityHeight = findViewById(R.id.scanner_fragment).getHeight();
-
-        int usableWidth = layoutContent.getWidth();
-        int usableHeight = activityHeight - layoutContent.getHeight();
-
-        int desiredHeight = (int) (usableHeight * 0.8);
-        int desiredWidth = (int) (usableWidth * 0.75);
-
-        Rect framingRect = new Rect(
-            (usableWidth - desiredWidth) / 2, // left
-            (usableHeight - desiredHeight) / 2, // top
-            (usableWidth - desiredWidth) / 2 + desiredWidth, // right
-            (usableHeight - desiredHeight) / 2 + desiredHeight// bottom
-        );
-        Log.v("RECT", "left: " + framingRect.left + " top: " + framingRect.top + " right: " + framingRect.right + " bottom: " + framingRect.bottom + " activityHeight: " + activityHeight + " activitiWidth: " + activityWidth);
-//        mScannerFragment.setFramingRect(framingRect);
-      }
-    });*/
-
-//    mToggleButton = (Button) findViewById(R.id.button_flash);
-//    mToggleButton.setOnClickListener(createToggleFlashListener());
-
     }
 
     @Override
